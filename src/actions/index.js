5a63eb47cbde9ed9b235f28e2352fb2ff48052d1
Joshua Rabiu <joshua.rabiu@gmail.com>
2019-01-03 20:46:44 +0000

    single-line ternary expressions

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,17 +15,11 @@ export const postTheme = () => {
 
     const isValidFirefoxColorLink = link => !!link.startsWith('https://color.firefox.com/?theme=');
 
-    doesInputHaveValue(authorName) 
-      ? dispatch({ type: 'VALID_AUTHOR_NAME' }) 
-      : dispatch({ type: 'EMPTY_AUTHOR_NAME' });
+    doesInputHaveValue(authorName) ? dispatch({ type: 'VALID_AUTHOR_NAME' }) : dispatch({ type: 'EMPTY_AUTHOR_NAME' });
 
-    doesInputHaveValue(themeName)
-      ? dispatch({ type: 'VALID_THEME_NAME' })
-      : dispatch({ type: 'EMPTY_THEME_NAME' });
+    doesInputHaveValue(themeName) ? dispatch({ type: 'VALID_THEME_NAME' }) : dispatch({ type: 'EMPTY_THEME_NAME' });
 
-    isValidFirefoxColorLink(themeLink)
-      ? dispatch({ type: 'VALID_LINK' })
-      : dispatch({ type: 'INVALID_LINK' });
+    isValidFirefoxColorLink(themeLink) ? dispatch({ type: 'VALID_LINK' }) : dispatch({ type: 'INVALID_LINK' });
 
     if (
       !store.getState().isLinkInvalid &&
